ea832ed274729012955a259a09ee18884b279ad0
Sumant Chahil <schahil31@gmail.com>
2017-06-25 21:24:05 -0700

    Modified the fiel LinkedList.java
    -Added a Node class to create a new node for each add value in the list
    -Added the methods to add to the end of the list and to a location
    inside the list
    -Added the methods getSize and setSize
    	-getSize will give the number of elements in the list
    	-setSize will set the list to a certain size and give each
    	element a null value

--- LinkedList.java
+++ LinkedList.java
@@ -1,63 +1,97 @@
 
 public class LinkedList<T>{
 
-	private Node<T> head, tail;
+	private Node<T> head;
 	private int size;
-	
-	public LinkedList() {
-		head = null;
-		tail = null;
+
+	public LinkedList(){
+		head = new Node<T>(null);
 		size = 0;
 	}
-	
-	public void add(T item) {
-		
-		if(isEmpty()) {
-			tail = new Node<T>(item, null);
-			head = tail;
-		} 
-		else {
-			Node<T> previous = tail;
-			tail = new Node<T>(item, null);
-			previous.next = tail;
+
+	public void print(){
+		Node<T> current = head;
+		while(current.next != null){
+			System.out.print(current.data + " -> ");
+			current = current.next;
 		}
-		
-		size++;
+		System.out.println(current.data);
 	}
-	
-	public boolean isEmpty() {
-		return size == 0;
-	}
-	
-	public void setSize(int size) {
-		this.size = size;
-	}
-	
-	public int getSize() {
-		return size;
-	}
-	
-	
-	private class Node<T>{
 
-	    public T data;
-	    Node<T> next;
+    public void add(T d){
+    	Node<T> end = new Node<T>(d);
+    	Node<T> current = head;
 
-	    public Node(T data, Node<T> next) {
-	        this.data = data;
-	        this.next = next;
-	    }
+    	while(current.next != null){
+    		current = current.next;
+    	}
+    	current.next = end;
+    	size++;
+        System.out.println(d + " appended to tail!");
+    }
 
-	}
-	
-	
-	public static void main(String[] args) {
-		
-		LinkedList<String> list = new LinkedList<>();
-		
-		list.add("Hello");
-		list.add("World");
-		
-		System.out.println("The size of the list is "+ list.getSize());
-	}
+    public void add(T d,int index){
+    	Node<T> end = new Node<T>(d);
+    	Node<T> current = head;
+    	int jump;
+    	
+    	
+    	if(index > size || index < 1){
+    		System.out.println("Add Failed: index out of bounds of size of linked list!!");
+    	}
+    	else{
+    		jump = 0;
+    		
+    		while(jump < index - 1){
+    			current = current.next;
+    			jump++;
+    		}
+    		
+    		end.next = current.next;
+    		current.next = end;
+    		size++;
+    		System.out.println("Success! " + d + " added at index " + index);
+    	}
+    }
+    
+    public int getSize() {
+    	return size;
+    }
+    
+    public void setSize(int size) {
+    	for(int i = 0; i < size; i++) {
+    		add(null);
+    	}
+    }
+    
+    public static void main(String[] args) {
+    	
+    	LinkedList<Integer> list = new LinkedList<Integer>();
+    	LinkedList<String> strList = new LinkedList<String>();
+    	LinkedList<Integer> emptyList = new LinkedList<Integer>();
+    	
+    	emptyList.setSize(5);
+    	emptyList.print();
+    	System.out.println();
+    	
+    	strList.add("Here's");
+    	strList.add("Johnny2");
+    	strList.add("Johnny1", 2);
+    	strList.print();
+    	
+    	System.out.println();
+    	list.add(1);
+    	list.add(4);
+    	list.add(5590);
+    	list.print();
+    }
+}   
+
+class Node<T> {
+	Node<T> next = null;
+   	T data;
+
+   	public Node(T d){
+	   	data = d;
+    }
 }
