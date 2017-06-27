6c2c5f174bbea272580ee17db54e792a71dd1354
Sumant Chahil <schahil31@gmail.com>
2017-06-27 12:18:22 -0700

    Modified the file DoubleLinkedList.java
    -Added the methods addFront, add, removeEnd, removeFront, remove, and
    getSize
    -addFront adds a node in front of the head element and makes it the new
    head element
    -add adds a node at the index inputted to the method
    -removeEnd deletes the node from the list that has the next pointer
    pointing at null
    -removeStart deletes the node from the list that has the previous
    pointer pointing at null
    -remove deletes a node at the index inputted to the method
    -getSize returns the size of the element

--- Homework_06_24_17/DoubleLinkedList.java
+++ Homework_06_24_17/DoubleLinkedList.java
@@ -1,8 +1,8 @@
 
 public class DoubleLinkedList<T> {
 	
-	DoubleNode<T> head;
-	int size;
+	private DoubleNode<T> head;
+	private int size;
 	
 	public DoubleLinkedList() {
 		head = new DoubleNode<T>(null);
@@ -19,7 +19,7 @@ public class DoubleLinkedList<T> {
 		}
 		System.out.println(current.value);
 		
-		System.out.println("The size of the list is " + size);
+		System.out.println("The size of the list is: " + getSize());
 	}
 	
 	public void printStack() {
@@ -35,7 +35,7 @@ public class DoubleLinkedList<T> {
 			current = current.previous;
 		}
 		System.out.println(current.value);
-		System.out.println("The size of the list is: " + size);
+		System.out.println("The size of the list is: " + getSize());
 	}
 	
 	public void addEnd(T v) {
@@ -73,6 +73,81 @@ public class DoubleLinkedList<T> {
 		size++;
 	}
 	
+	public void add(T v, int index) {
+		DoubleNode<T> input = new DoubleNode<T>(v);
+		DoubleNode<T> current = head;
+		int jump;
+		
+		if(index > size || index < 1) {
+			System.out.println("Error: Out of bounds");
+		}
+		else {
+			jump = 0;
+			
+			while(jump < index - 1) {
+				current = current.next;
+				jump++;
+			}
+			input.next = current.next;
+			input.previous = current;
+			current.next = input;
+			current = input.next;
+			current.previous = input;
+			size++;
+		}
+	}
+	
+	public void removeEnd() {
+		DoubleNode<T> current = head;
+		
+		while(current.next != null) {
+			current = current.next;
+		}
+		
+		DoubleNode<T> temp = current.previous;
+		current.previous = null;
+		temp.next = null;
+		size--;
+	}
+	
+	public void removeFront() {
+		DoubleNode<T> current = head;
+		
+		head = current.next;
+		head.previous = null;
+		current.next = null;
+		size--;
+	}
+	
+	public void remove(int index) {
+		DoubleNode<T> current = head;
+		int jump;
+		
+		if(index > size || index < 1) {
+			System.out.println("Error: Out of Bounds");
+		}
+		else {
+			jump = 0;
+			
+			while(jump < index - 1) {
+				current = current.next;
+				jump++;
+			}
+			
+			DoubleNode<T> remove = current.next;
+			DoubleNode<T> nextNode = remove.next;
+			current.next = nextNode;
+			nextNode.previous = current;
+			remove.next = null;
+			remove.previous = null;
+			size--;
+		}
+	}
+	
+	public int getSize() {
+		return size;
+	}
+	
 	public static void main(String[] args) {
 		DoubleLinkedList<Integer> list = new DoubleLinkedList<Integer>();
 		
@@ -83,6 +158,10 @@ public class DoubleLinkedList<T> {
 		list.addEnd(6);
 		list.addFront(159);
 		list.addFront(0);
+		list.add(50, 4);
+		list.removeEnd();
+		list.removeFront();
+		list.remove(4);
 		list.printQueue();
 		System.out.println();
 		list.printStack();
