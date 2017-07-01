c87295a300de8f0a023d361041432ebba2922579
Sumant Chahil <schahil31@gmail.com>
2017-06-30 20:52:06 -0700

    Modified the files Queue.java and QueueTest.java
    -Added the print methods as well as size methods
    -User can print the list forward and in reverse
    -As well as getSize of the current list
    -Finished the methods initialize and push
    -The initialize method allows the user to input a node at the beginning
    of the list
    -The push method allows the user to enter a node anywhere after the
    beginning of the list

--- Homework_06272017/Queue.java
+++ Homework_06272017/Queue.java
@@ -1,41 +1,165 @@
+import java.util.Scanner;
 
 public class Queue {
+	Scanner input = new Scanner(System.in);
 	QueueNode head;
 	
+	//Constructor initializes class variables
 	public Queue() {
 		head = new QueueNode(null);
 	}// end of constructor Queue
 	
-	public void initialize(String value) {
+	//prints values how they are put in
+	public void printForward() {
+		QueueNode current = head;
 		
+		while(current.next != null) {
+			System.out.print(current.value + " -> ");
+			current = current.next;
+		}
+		System.out.println(current.value);
+	}//end of method printForward
+	
+	//prints values in the opposite direction
+	public void printBackward() {
+		QueueNode current = head;
+		
+		while(current.next != null) {
+			current = current.next;
+		}
+		
+		System.out.print(current.value);
+		while(current.next != null) {
+			current = current.next;
+			System.out.print(" <- " + current.value);
+		}
+		System.out.println();
+	}//end of method printBackward 
+	
+	//sets the size of the list
+	private int setSize(int size) {
+		QueueNode current = head;
+		
+		if(current.value != null) {
+			size++;
+		}
+		while(current.next != null) {
+			current = current.next;
+			size++;
+		}
+		return size;
+	}//end of method setSize
+
+	//gets the size of the list and returns it 
+	public int getSize() {
+		int size = 0;
+		size = setSize(size);
+		return size;
+	}//end of method getSize
+	
+	//initializes the Queue and adds values to the front 
+	public void initialize(String value) {
+		QueueNode newNode = new QueueNode(value);
+		QueueNode current = head;
+		
+		if(current.value == null) {
+			current.value = value;
+			return;
+		}
+		
+		newNode.next = current;
+		current.previous = newNode;
+		head = newNode;
 	}//end of method initialize
 	
+	//adds a node between the 1st and last index 
 	public void push(String value, int index) {
+		QueueNode newNode = new QueueNode(value);
+		QueueNode current = head;
 		
+		if(index < 1 || index > getSize()) {
+			System.out.println("Error: Out of Bounds");
+			return;
+		}
+		if(head.value == null) {
+			System.out.println("Error: List not Initialized");
+			return;
+		}
+		
+		switch(index) {
+		case 1:
+			newNode.next = current.next;
+			newNode.previous = current;
+			current.next.previous = newNode;
+			current.next = newNode;
+			return;
+		default:
+			if(index == getSize()) {
+				while(current.next != null) {
+					current = current.next;
+				}
+				current.next = newNode;
+				newNode.previous = current;
+				return;
+			}//adds value to end of list
+			int jump = 0;
+			while(jump < index - 1) {
+				current = current.next;
+				jump++;
+			}
+			newNode.next = current.next;
+			newNode.previous = current;
+			current.next.previous = newNode;
+			current.next = newNode;
+			return;
+		}//end of switch statement
 	}//end of method push
 	
+	//deletes a node in the list given the position beginning is 1 and end value is the size of the list
 	public void pop(int index) {
 		
 	}//end of method pop
 	
+	//deletes the entire list
 	public void destroy() {
 		head.next = null;
 		head.value = null;
 	}//end of method destroy
 	
+	//changes a value of any node in the list given the index
 	public void editNode(String value, int index) {
 		
 	}//end of method editNode
 	
+	//gives a user interface for the user to create and interact with the list
 	public void menu() {
+		int choose = 0, index;
+		String value;
 		
+		while(choose != 6) {
+			//Displays Menu
+			System.out.println("-----------------------------");
+			System.out.println("Welcome to Create LinkedList");
+			System.out.println("-----------------------------");
+			System.out.println("Pick one of the following options:");
+			System.out.println("1. Initialize the List");
+			System.out.println("2. Add node to the List");
+			System.out.println("3. Remove node from the List");
+			System.out.println("4. Delete the List");
+			System.out.println("5. Print the List");
+			System.out.println("6. Quit");
+			choose = input.nextInt();
+			input.nextLine();
+		}//end of while loop
 	}//end of menu
 }//end of class Queue
 
+//creates a new node in the Queue
 class QueueNode {
 	QueueNode next, previous;
 	String value;
 	
+	//initializes the node with next and previous pointer to null and the value will be user input
 	public QueueNode(String value) {
 		this.value = value;
 		next = null;
--- Homework_06272017/QueueTest.java
+++ Homework_06272017/QueueTest.java
@@ -8,7 +8,16 @@ public class QueueTest {
 	
 	@Test
 	public void test() {
+		queue.initialize("hey");
+		queue.initialize("hi");
+		queue.initialize("hey");
+		queue.initialize("hi");
+		queue.initialize("hey");
+		queue.initialize("hi");
+		queue.push("zuzu", 6);
+		queue.printForward();
 		
+		assertEquals(7, queue.getSize());
 	}
 
 }
