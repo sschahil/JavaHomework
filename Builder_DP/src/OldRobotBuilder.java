94eab5ee91def025b1cf25fb58a3ab92a530e134
sschahil <schahil31@gmail.com>
2017-08-01 00:57:16 -0700

    Added the Builder Design Pattern
    -This is a Creational Pattern
    -Separates the construction of a complex object from its representation
    -Allowing the same construction process to create various
    representations

---
+++ Builder_DP/src/OldRobotBuilder.java
@@ -0,0 +1,35 @@
+
+public class OldRobotBuilder implements RobotBuilder{
+	
+	private Robot robot;
+	
+	public OldRobotBuilder() {
+		this.robot = new Robot();
+	}
+	
+	@Override
+	public void buildRobotHead() {
+		robot.setRobotHead("Tin Head");
+	}
+
+	@Override
+	public void buildRobotTorso() {
+		robot.setRobotTorso("Tin Torso");
+	}
+
+	@Override
+	public void buildRobotArms() {
+		robot.setRobotArms("Blowtorch Arms");
+	}
+
+	@Override
+	public void buildRobotLegs() {
+		robot.setRobotLegs("Rollar Skates");
+	}
+
+	@Override
+	public Robot getRobot() {
+		return this.robot;
+	}
+
+}
