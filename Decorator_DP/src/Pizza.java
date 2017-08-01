524619af9b3701b892a63704edaba37ee881f8a3
sschahil <schahil31@gmail.com>
2017-08-01 01:00:28 -0700

    Added the Decorator Design Pattern
    -This is a Structural Pattern
    -Attach additional responsibilities to an object dynamically keeping the
    same interface
    -Decorators provide flexible alternative to sub classing for extending
    functionality

---
+++ Decorator_DP/src/Pizza.java
@@ -0,0 +1,14 @@
+
+public interface Pizza {
+	public String getDescription();
+	
+	public double getCost();
+}
+
+//public abstract class Pizza {
+//	public abstract void setDescription(String newDescription);
+//	public abstract String getDescription();
+//	
+//	public abstract void setCost(double newCost);
+//	public abstract double getCost();
+//}
---
+++ Decorator_DP/src/ThreeCheesePizza.java
@@ -0,0 +1,22 @@
+
+public class ThreeCheesePizza extends Pizza{
+	
+	private String description = "Mozzarella, Fontina, Parmesan Cheese Pizza";
+	private double cost = 10.50;
+	
+	public void setDescription(String newDescription) {
+		description = newDescription;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setCost(double newCost) {
+		cost = newCost;
+	}
+	
+	public double getCost() {
+		return cost;
+	}
+}
